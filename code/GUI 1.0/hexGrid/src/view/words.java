0b06263f04f5b4be74b73e52ead0f20c36e5b7f6
Yirui Yue (yiy730) <yiy730@mail.usask.ca>
2016-12-07 22:23:13 -0600

    added some comments to the words class

--- code/GUI 1.0/hexGrid/src/view/words.java
+++ code/GUI 1.0/hexGrid/src/view/words.java
@@ -6,15 +6,32 @@ public class words {
 
 	private static Map<String,String> wordDictionary;
 	
+
+	/**
+     * @consturctor  
+     **/	
 	public words(){
 		wordDictionary = new HashMap<String,String>();
 	}
 	
+
+
+	/**
+	 * @pre nothing
+	 * @post nothing
+     * @return the word dictionary
+     **/	
 	public static Map<String,String> getDictionary(){
 		return wordDictionary;
 	}
 	
 	
+
+	/**
+	 * @pre the word name to be difined, the script 
+	 * @post define the input word if the word is not defined before
+     * @return nothing  
+     **/	
 	public static void defineWord( String name, String script){
 		
 		if(wordDictionary.get(name)== null){
@@ -24,10 +41,24 @@ public class words {
 		}
 	}
 	
+
+	/**
+	 * @pre the word name to be removed 
+	 * @post the input word is removed
+     * @return nothing  
+     **/	
 	public static void removeWord(String word){
 		wordDictionary.remove(word);
 	}
 	
+
+
+
+	/**
+	 * @pre the word key
+	 * @post nothing
+     * @return return the word matches the given key
+     **/
 	public static String findWord(String key){
 		return wordDictionary.get(key);
 	}
