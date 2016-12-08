97dc5b1797c5032540e25dc6f4e0f168f9df2e9b
Yirui Yue (yiy730) <yiy730@mail.usask.ca>
2016-12-08 12:08:48 -0600

    added some comments to soundClip class

--- code/GUI 1.0/hexGrid/src/view/SoundClip.java
+++ code/GUI 1.0/hexGrid/src/view/SoundClip.java
@@ -1,5 +1,3 @@
-
-
 package view;
 
 import java.io.File;
@@ -22,42 +20,68 @@ public class SoundClip {
 	public AudioInputStream audioIn2;
 	public SoundClip(String file1,String file2) {
 		try{
-		soundFile = new File(file1);
-        attackFile = new File(file2);
-		// Get a sound clip resource.
-	    clip1 = AudioSystem.getClip();
-	    clip2 =AudioSystem.getClip();
-        // Open an audio input stream.
-
-		 audioIn1 = AudioSystem.getAudioInputStream(this.soundFile);
-         // Open an audio input stream.
-
-		 audioIn2 = AudioSystem.getAudioInputStream(this.attackFile);
-         clip1.open(audioIn1);
-         clip2.open(audioIn2);
+	        soundFile = new File(file1);
+            attackFile = new File(file2);
+		    // Get a sound clip resource.
+	        clip1 = AudioSystem.getClip();
+	        clip2 =AudioSystem.getClip();
+            // Open an audio input stream.
+     		audioIn1 = AudioSystem.getAudioInputStream(this.soundFile);
+             // Open an audio input stream.
+      		audioIn2 = AudioSystem.getAudioInputStream(this.attackFile);
+            clip1.open(audioIn1);
+            clip2.open(audioIn2);
 		}catch (UnsupportedAudioFileException e) {
 	         e.printStackTrace();
-	      }catch (IOException e) {
+	    }catch (IOException e) {
 		         e.printStackTrace();
-		      }catch (LineUnavailableException e) {
+		}catch (LineUnavailableException e) {
 			         e.printStackTrace();
-			      }
+	    }
 	}
-	  public void soundPlay(){
 
-		      clip1.loop(Clip.LOOP_CONTINUOUSLY);
-		   }
-	  public void attackPlay(){
-		   clip2.setMicrosecondPosition(0);
-		    clip2.start();
-	  }
-	  public void stopAttackSound(){
-		  if (clip2.isRunning())
-		    clip2.stop();
+
+	/**
+	 * @pre nothing
+	 * @post contimuously play the main game sound
+     * @return nothing
+     **/		  
+	public void soundPlay(){
+
+		clip1.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+	  
+
+	/**
+	 * @pre nothing
+	 * @post play the shoot sound while attacking another robot
+     * @return nothing
+     **/
+	public void attackPlay(){
+		 clip2.setMicrosecondPosition(0);
+		 clip2.start();
+	}
+
+
+	/**
+	 * @pre nothing
+	 * @post stop the shoot sound while attack is finished
+     * @return nothing
+     **/
+	 public void stopAttackSound(){
+		if (clip2.isRunning())
+		clip2.stop();
 		  
-	  }
-	  public  void stopPlay(){
-		   clip1.stop();
-		   clip1.setMicrosecondPosition(0);
+	}
+
+
+	/**
+	 * @pre nothing
+	 * @post stop the main game sound when the game is over
+     * @return nothing
+     **/
+	public  void stopPlay(){
+		clip1.stop();
+		clip1.setMicrosecondPosition(0);
 	  }
 }
